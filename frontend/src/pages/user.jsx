b6e1efe65faaef28fe15bb128c237827bef9e42e
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-14 09:59:22 +0200

    Se pueden eliminar citas y se actualiza de forma automatica

--- frontend/src/pages/user.jsx
+++ frontend/src/pages/user.jsx
@@ -1,8 +1,8 @@
 import { userLogin } from "../api/apiUser";
-import { getCita } from "../api/apiCita.js";
+import { getCita, citaDelete } from "../api/apiCita.js";
 import { getPropiedad } from '../api/apiPropiedad.js'
 import { Navbar } from "../componentes/navbar.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Cookies from 'js-cookie';
 import { sha256 } from 'js-sha256';
@@ -11,49 +11,58 @@ import '../scss/user.scss'
 
 function UserPanel() {
     const [listCitas, setListCitas] = useState('')
+    const [estadoBoton, setEstadoBoton] = useState(true);
 
 
 
+    useEffect(() => {
+        if (estadoBoton) {
+            Cita()
+            async function Cita() {
+                let listCita = await getCita()
+                listCita = await listCita.data
 
-    Cita()
-    async function Cita() {
-        let listCita = await getCita()
-        listCita = await listCita.data
+                let listPropiedad = await getPropiedad()
+                listPropiedad = await listPropiedad.data
 
-        let listPropiedad = await getPropiedad()
-        listPropiedad = await listPropiedad.data
-
-        const name_ = Cookies.get('username')
-        let listCitaArray = []
-        let listPropiedadArray = []
+                const name_ = Cookies.get('username')
+                let listCitaArray = []
+                let listPropiedadArray = []
 
 
-        for (let i = 0; listCita.length > i; i++) {
-            if (name_ == sha256(listCita[i].username)) {
-                listCitaArray.push(listCita[i])
+                for (let i = 0; listCita.length > i; i++) {
+                    if (name_ == sha256(listCita[i].username)) {
+                        listCitaArray.push(listCita[i])
 
-                for (let j = 0; listPropiedad.length > j; j++) {
-                    if (listCita[i].propiedad == listPropiedad[j]._id) {
-                        listPropiedadArray.push(listPropiedad[j])
+                        for (let j = 0; listPropiedad.length > j; j++) {
+                            if (listCita[i].propiedad == listPropiedad[j]._id) {
+                                listPropiedadArray.push(listPropiedad[j])
+                            }
+                        }
                     }
                 }
+                
+                const citasDiv = await listCitaArray.map((data) => 
+                    <div id='div-citas' key={data._id}>
+                        <p>Fecha: {data.date}</p>
+                        <p>Vendedor: {data.vendor}</p>
+                        <input id={data._id} type="button" value="Eliminar cita" onClick={citaDeleteButton} />
+                    </div>
+                )
+                setListCitas(citasDiv)
+                setEstadoBoton(false)
             }
         }
+    }, [estadoBoton]);
+
+
+
+
+    async function citaDeleteButton(e) {
+        const propiedadID = e.target.id
+        const citaCrear = await citaDelete(propiedadID)
+        setEstadoBoton(true);
         
-
-        const citasDiv = await listCitaArray.map((data) => 
-            <div id='div-citas' key={data._id}>
-                <p>Fecha: {data.date}</p>
-                <p>Vendedor: {data.vendor}</p>
-                <input type="button" value="Eliminar cita" onClick={citaDelete} />
-            </div>
-        )
-        setListCitas(citasDiv)
-    }
-
-
-    async function citaDelete(e) {
-        //Aqui para eliminar la cita
     }
 
 
