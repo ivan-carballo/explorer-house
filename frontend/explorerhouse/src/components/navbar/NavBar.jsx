4810cc98168f6abb1f5dba643fce371a2308731a
aitormon <aitormon@gmail.com>
2024-06-14 10:49:43 +0200

    Navbar

--- frontend/explorerhouse/src/components/navbar/NavBar.jsx
+++ frontend/explorerhouse/src/components/navbar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import './Cabecera.css';
+import './NavBar.css';
 import { FaShoppingCart, FaHome, FaUser, FaUserPlus, FaBuilding, FaMapMarkerAlt, FaWarehouse, FaAddressBook, FaCoins, FaUserMinus } from 'react-icons/fa';
 
 
 
-const Cabecera = () => {
+const NavBar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -28,46 +28,47 @@ const Cabecera = () => {
     <header className="cabecera">
       <ul className="cabecera__nav">
         <li className="cabecera__left">
-          <img src="src/components/cabecera/explorerhouselogo.jpg" alt="Logo" className="cabecera_icon" />
+          <img src="src/components/navbar/explorerhouselogo.jpg" alt="Logo" className="cabecera_icon" />
         </li>
         <li>
-          <button onClick={() => handleNavigation('/')}><FaHome /> Inicio</button>
+          <button className="boton"onClick={() => handleNavigation('/')}><FaHome /> Inicio</button>
         </li>
         <li className="dropdown">
-  <button onClick={toggleDropdown} className="dropdown__toggle">
+  <button onClick={toggleDropdown} className="dropdown__toggle boton">
     <FaBuilding /> Inmuebles
   </button>
   {dropdownOpen && (
     <ul className="dropdown__menu">
+      <li><b><p  className="texto">Buscar por precio</p></b></li>
       <li><button onClick={() => buscaporprecio(70000,150000)}><FaCoins /> 70000-150000</button></li>
       <li><button onClick={() => buscaporprecio(150001,300000)}><FaCoins /> 150001-300000</button></li>
       <li><button onClick={() => buscaporprecio(300001,500000)}><FaCoins /> 300001-500000</button></li>
-      <li><button onClick={() => buscaporprecio(500001,2000000)}><FaCoins /> 50001+</button></li>
+      <li><button onClick={() => buscaporprecio(500001,2000000)}><FaCoins /> 500001 +</button></li>
     </ul>
   )}
 </li>
         <li>
-          <button onClick={() => handleNavigation('/login')}><FaUserPlus /> Login/Register </button>
+          <button className="boton" onClick={() => handleNavigation('/login')}><FaUserPlus /> Login/Register </button>
         </li>
         <li>
-          <button onClick={() => handlelogout()}><FaUserMinus /> Logout</button>
+          <button className="boton" onClick={() => handlelogout()}><FaUserMinus /> Logout</button>
         </li>
         <li>
-        <button onClick={() => handleNavigation('/usuario')}>
-          <img src="src/components/cabecera/usuario.png" alt="Usuario" className="usuario" />
+        <button className="boton" onClick={() => handleNavigation('/usuario')}>
+          <img src="src/components/navbar/usuario.png" alt="Usuario" className="usuario" />
           </button>
         </li>
         <li>
-        <img src="src/components/cabecera/explorerhouselogo.jpg" alt="Logo" className="cabecera_icon" />
+        <img src="src/components/navbar/explorerhouselogo.jpg" alt="Logo" className="cabecera_icon" />
         </li>
       </ul>
       
     </header>
-    <img src="src/components/cabecera/linea.jpg" alt="" className="linea"/>
+    <img src="src/components/navbar/linea.jpg" alt="" className="linea"/>
     <br></br>
     <br></br>
     </>
   );
 }
 
-export default Cabecera;
+export default NavBar;
