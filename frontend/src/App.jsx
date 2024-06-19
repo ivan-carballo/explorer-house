9169660f71a1b46d6169d3e17d530b76f09d1bbd
aitor <aitormon@gmail.com>
2024-06-19 19:54:05 +0200

    juntando mi trabajo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import router from './router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import NavBar from './components/navbar/NavBar.jsx'
+import Inmuebles from './components/Inmuebles.jsx'
 
 function App() {
 
+  
+  const [inmuebles, setInmuebles] = useState([]);
  
   return (
     <>
         <RouterProvider router={router} />
+
+{/*         <NavBar setInmuebles={setInmuebles} />
+      
+      º<Inmuebles inmuebles={inmuebles} /> */}
     </>
   )
 }
