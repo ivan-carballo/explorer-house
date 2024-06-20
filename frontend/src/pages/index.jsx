9a932937db1ecaa10e266b057d2a3043cde99f72
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-20 12:53:02 +0200

    Poner en el nombre del boton de pedir cita el estado de la cita ya pedida anteriormente

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -34,29 +34,60 @@ function Index() {
 
                 const getCitas = await getCita()
                 let getArrayCitas = await getCitas.data
-                let userCitas = '';
+
+                let userCitas = [];
+                let userCitasUni = [];
                 
 
                 for (let i = 0; getArrayCitas.length > i; i++) {
                     if (sha256(getArrayCitas[i].username) == user) {
-                        userCitas += getArrayCitas[i].propiedad
+                        userCitasUni.push(getArrayCitas[i].propiedad)
+                        userCitasUni.push(getArrayCitas[i].state)
                     }
+                    userCitas.push(userCitasUni)
+                    userCitasUni = []
                 }
 
+                let userCitasFiltrado = userCitas.filter(objeto => Object.keys(objeto).length !== 0);
 
-                const propiedadesDiv = await getArray.map((data) =>
-                    <div id='div-pisos' key={data._id}>
-                        <h1>{data.tipo} en {data.ciudad}</h1>
-                        <p id='descripcion'>{data.descripcion}</p>
-                        <img id='img-pisos' src={data.imagen != null ? data.imagen: '../../public/noPhoto.avif'} />
+                let propiedadesArray = []
+                let propiedadesArrayUni = []
+
+                for (let i = 0; getArray.length > i; i++) {
+                    propiedadesArrayUni.push(getArray[i]._id) //0
+                    propiedadesArrayUni.push(getArray[i].tipo) //1
+                    propiedadesArrayUni.push(getArray[i].ciudad) //2
+                    propiedadesArrayUni.push(getArray[i].descripcion) //3
+                    propiedadesArrayUni.push(getArray[i].imagen) //4
+                    propiedadesArrayUni.push(getArray[i].habitaciones) //5
+                    propiedadesArrayUni.push(getArray[i].metros) //6
+                    propiedadesArrayUni.push(getArray[i].altura) //7
+                    propiedadesArrayUni.push(getArray[i].precio) //8
+                    propiedadesArrayUni.push(getArray[i].vendor) //9
+                    
+                    for (let j = 0; userCitasFiltrado.length > j; j++) {
+                        if (userCitasFiltrado[j][0] == getArray[i]._id) {
+                            propiedadesArrayUni.push(userCitasFiltrado[j][1])
+                        }
+                    }
+                    propiedadesArray.push(propiedadesArrayUni)
+                    propiedadesArrayUni = []
+                }
+                
+
+                const propiedadesDiv = await propiedadesArray.map((data) =>
+                    <div id='div-pisos' key={data[0]}>
+                        <h1>{data[1]} en {data[2]}</h1>
+                        <p id='descripcion'>{data[3]}</p>
+                        <img id='img-pisos' src={data[4] != null ? data[4]: '../../public/noPhoto.avif'} />
                         <ul>
-                            <li>Habitaciones: {data.habitaciones}</li>
-                            <li>Metros: {data.metros}</li>
-                            <li>Altura: {data.altura}</li>
-                            <li>Precio: {data.precio} €</li>
-                            <li>Vendedor: {data.vendor}</li>
+                            <li>Habitaciones: {data[5]}</li>
+                            <li>Metros: {data[6]}</li>
+                            <li>Altura: {data[7]}</li>
+                            <li>Precio: {data[8]} €</li>
+                            <li>Vendedor: {data[9]}</li>
                         </ul>
-                        <input id={data._id} className="button-pisos" type="button" value={userCitas.includes(data._id) ? 'Cita solicitada' : (sha256(data.vendor) == Cookies.get('username') ? 'Es tu propiedad' : 'Pedir cita') } onClick={newCita} />
+                        <input id={data[0]} className="button-pisos" type="button" value={data[10] != undefined ? data[10] : (sha256(data[9]) == Cookies.get('username') ? 'Es tu propiedad' : 'Pedir cita') } onClick={newCita} />
                     </div>
                 )
                 setListado(propiedadesDiv)
